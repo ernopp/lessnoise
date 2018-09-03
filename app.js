f002873535848c48b81397b9c0a093a881e106ff
Ernest Oppetit <eoppetit@gmail.com>
2018-09-04 00:19:58 +0100

    no use for cookie-parser

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var createError = require('http-errors');
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var session = require('express-session');
