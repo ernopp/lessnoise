12461cac06e77df6cb5faf324b7ec6efee45fa24
Ernest Oppetit <eoppetit@gmail.com>
2018-10-21 21:58:51 +0100

    cursoring, getting rekt by async call in while loop

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
     
   consumer.get("https://api.twitter.com/1.1/account/verify_credentials.json", req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response) {
       if (error) {
-        console.log("error verifying creds: ", JSON.stringify(error));
+        console.log("error verifying creds trying to connect: ", JSON.stringify(error));
         res.redirect('/signin/connect');
       } else {        
         console.log("successfully verified creds. data: " , data);
@@ -24,38 +24,53 @@ router.get('/', function(req, res, next) {
         twitterIdentifier["screen_name"] = verifyCredentialsData["screen_name"];
 
         //get friends list 
-        consumer.get("https://api.twitter.com/1.1/friends/list.json", req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response){
-          if(error) {
-            console.log("Error getting friends, error: ", JSON.stringify(error));
-          }
-          else{                  
-            // console.log("successfully got friends ")
-            // console.log("------data---------")
-            // console.log(data)
-            // console.log("-------data.users--------")
-            // console.log(JSON.parse(data)["users"])
+      	//https://developer.twitter.com/en/docs/basics/cursoring
+		var nextcursorstr = "-1";        
+		var nextcursor = -1;
+		var rawFriendsList = {};		
 
-            var rawFriendsList = JSON.parse(data)["users"];            
-            var transformedFriendsList = utils.transformFriendsList(rawFriendsList);
-            var prettyFriendsList = utils.getPrettyFriendsList(transformedFriendsList);
+		 while(nextcursor !=0){
+			// console.log("nextcursor is " + nextcursor);
 
+			consumer.get("https://api.twitter.com/1.1/friends/list.json?cursor="+nextcursorstr, req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response){
+	          if(error) {
+	          	var e = JSON.stringify(error)
+	            console.log("Error getting friends, error: ", e);
+	            res.send("Hmm, you are authenticated but couldn't get back friends list from twitter api, error: "+e, 500)
+	          }
+	          else{                  
+	          	console.log("-------------GOT DATA BACK --------, number of objects: ", data["total_count"]);
+	          	console.log(data);
+	          	nextcursorstr = data["next_cursor_str"];	          		          	
+	          	nextcursor = data["next_cursor"];	          		          	
+
+	          	// rawFriendsList = rawFriendsList.concact(JSON.parse(data)["users"])
+	          	// console.log(JSON.stringify(rawFriendsList))
+			  }  
+			});
+		  }	       
+        		// var transformedFriendsList = utils.transformFriendsList(rawFriendsList);
+       			// var prettyFriendsList = utils.getPrettyFriendsList(transformedFriendsList);
+       			//   console.log(prettyFriendsList);
+
+     //      	  "next_cursor": 1613662072812786926,
+			  // "next_cursor_str": "1613662072812786926",
+			  // "previous_cursor": 0,
+			  // "previous_cursor_str": "0",
+			  // "total_count": null
+
+
+            // save to mongodb
             // dbo.collection("users").insertMany(transformedFriendsList, function(err, res) {
             //   if (err) throw err;
             //   console.log("Number of documents inserted: " + res.insertedCount);
             //   dbo.close();
             // });
-
-            console.log(prettyFriendsList);
-            
-            // https://expressjs.com/en/api.html#res.render
-            res.render('index', {title: 'LessNoise', prettyFriendsList: prettyFriendsList});
-          }
-        }); // consumer.get friends list         
-    }// else
-  });
-
+        
+        // https://expressjs.com/en/api.html#res.render
+        // res.render('index', {title: 'LessNoise', prettyFriendsList: prettyFriendsList});
+      }
+    }); // consumer.get friends list         
 });
-
-
 
 module.exports = router;
