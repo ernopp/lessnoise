228b72a25e9e85ee31ced819138069c699a7bcbd
Ernest Oppetit <ernopp@users.noreply.github.com>
2019-11-02 10:52:15 +0000

    heroku callbackstring

--- twitter-client.js
+++ twitter-client.js
@@ -1,11 +1,11 @@
 /* Twitter client - app settings: https://apps.twitter.com/app/5573851/settings */
 
-// require('dotenv').config()
 const oauth = require('oauth')
 
 const _twitterConsumerKey = process.env.KEY
 const _twitterConsumerSecret = process.env.SECRET
-const callbackString = "http://localhost:" + process.env.PORT  + "/signin/callback"
+const callbackString = process.env.NODE_ENV === "production" ? "https://glacial-ravine-68340.herokuapp.com/" :
+    "http://localhost:" + process.env.PORT  + "/signin/callback"
 
 module.exports = new oauth.OAuth(
   "https://twitter.com/oauth/request_token", "https://twitter.com/oauth/access_token",
