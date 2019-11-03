e454f9e93c856dd814f8be22b9b8ee8a6d5a96ad
Ernest Oppetit <ernopp@users.noreply.github.com>
2019-11-03 16:36:02 +0000

    error handling

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 
-const createError = require('http-errors')
 const logger = require('morgan')
 const session = require('cookie-session')
 const compression = require('compression')
@@ -54,19 +53,36 @@ app.use('/unfollow', unfollowRouter)
 
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
-})
+app.use(function(req, res, next) {
+    var err = new Error("Page not found.")
+    err.status = 404;
+    next(err);
+});
 
-// error handler
-app.use(function (err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message //https://expressjs.com/en/api.html#res.locals
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    debug("in dev error handler")
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,13 @@ const test = require('../test')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+
     consumer.get("https://api.twitter.com/1.1/account/verify_credentials.json", req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, async function (error, data, response) {
         if (error) {
             debug("error verifying creds trying to connect: ", JSON.stringify(error))
             res.render('home', {title: '🙏 LessNoise'})
 
         } else {
-
             try{
                 let loggedInUser = utils.getLoggedInUser(data)
 
@@ -24,32 +24,28 @@ router.get('/', function (req, res, next) {
                 let prettyFriendsList = (process.env.USETESTDATA === "true")
                     ? await test.getTestFriends() : await getFriends(req.session.oauthAccessToken, req.session.oauthAccessTokenSecret)
 
-                debug("prettyFriendsList  : " + prettyFriendsList)
+                if(!prettyFriendsList.error){
+                    res.render('reccos', {title: '🙏 LessNoise', loggedInUser: loggedInUser, prettyFriendsList: prettyFriendsList.data, baseURL: utils.baseURL})
+                }
+                else{
+                    debug("throwing rate limiting error: " + JSON.stringify(prettyFriendsList.error))
 
-                res.render('reccos', {title: '🙏 LessNoise', loggedInUser: loggedInUser, prettyFriendsList: prettyFriendsList, baseURL: utils.baseURL})
+                    let e = new Error("Sorry, you've been rate-limited by the Twitter API. Please try again in 15 mins (just reload this page)")
+                    e.status = 419
+                    next(e)
+                }
             }
             catch (err) {
-                return next(err)
+                next(err)
             }
-
         }
     })
 })
 
-router.get('/test-cookie', function(req, res) {
-    if (req.session.page_views) {
-        req.session.page_views++;
-        res.send("You visited this page " + req.session.page_views + " times");
-    } else {
-        req.session.page_views = 1;
-        res.send("Welcome to this page for the first time!");
-    }
-})
-
 /*
-Can do 15 requests per 15min
-  window https://developer.twitter.com/en/docs/basics/rate-limiting.html
-  Use count=200 to get max friends in friends list call https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/get-friends-list.html
+    Can do 15 requests per 15min
+    window https://developer.twitter.com/en/docs/basics/rate-limiting.html
+    Use count=200 to get max friends in friends list call https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/get-friends-list.html
  */
 
 async function getFriends(accesstoken, accesstokensecret) {
@@ -57,13 +53,15 @@ async function getFriends(accesstoken, accesstokensecret) {
     let nextcursor = -1
     let rawFriendsList = []
     let callCount = 0
+    let prettyFriendsList = []
 
-    // cap at 1000 recommendations - 5 calls of 200 each
-    while (nextcursor !== 0 && callCount <= 4) {
-        debug("-----callcount is ", callCount)
-        debug("-----nextcursor is ", nextcursor)
-        debug("-----rawfriendslist has size ", rawFriendsList.length)
-        try {
+    debug("New invocation of getFriends")
+
+    try {
+        // cap at 1000 recommendations - 5 calls of 200 each
+        while (nextcursor !== 0 && callCount <= 4) {
+            debug("-----cursoring: callcount is ", callCount)
+            debug("-----rawfriendslist has size ", rawFriendsList.length)
             const twitterResponse = await makeFriendsListCall(
                 nextcursor,
                 accesstoken,
@@ -73,21 +71,24 @@ async function getFriends(accesstoken, accesstokensecret) {
             callCount++
 
             for (let i = 0; i < twitterResponse["users"].length; i++) {
-                debug("adding friend " + twitterResponse["users"][i]["screen_name"])
+                // debug("adding friend " + twitterResponse["users"][i]["screen_name"])
                 rawFriendsList.push(twitterResponse.users[i])
             }
-        } catch (err) {
-            return next(err)
         }
+    }
+    catch (err){
+        debug("throwing error in getFriends: " + JSON.stringify(err))
+        prettyFriendsList.error = err
     }
 
     debug("-----Finished cursoring-----")
 
-    debug("-----callcount is ", callCount)
     debug("rawFriendsList has size : ", rawFriendsList.length)
 
     let augmentedFriendsList = utils.augmentFriendsList(rawFriendsList)
-    let prettyFriendsList = utils.getPrettyFriendsList(augmentedFriendsList)
+    prettyFriendsList.data = utils.getPrettyFriendsList(augmentedFriendsList)
+
+    debug("Finished invocation of getFriends")
 
     return prettyFriendsList
 }
@@ -99,7 +100,9 @@ function makeFriendsListCall(cursor, oauthAccessToken, oauthAccessTokenSecret) {
             "https://api.twitter.com/1.1/friends/list.json?cursor=" + cursor + "&count=200&skip_status=true&include_user_entities=false",
             oauthAccessToken,
             oauthAccessTokenSecret,
-            function (error, d) {
+            function (error, d, response) {
+
+                debug("response headers from friedns list call: " +JSON.stringify(response.headers))
                 let data = JSON.parse(d)
                 if (error) {
                     const e = JSON.stringify(error)
@@ -113,13 +116,8 @@ function makeFriendsListCall(cursor, oauthAccessToken, oauthAccessTokenSecret) {
             })
     })
 
-    promise.catch(function(error) {
-        console.error(error)
-        return reject(error)
-    });
-
-
-    return promise;
+    debug("contents promise are " +JSON.stringify(promise))
+    return promise
 }
 
 module.exports = router
