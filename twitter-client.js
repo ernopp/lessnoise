31d1c379090ab7cef0273095e29874968bd28baa
Ernest Oppetit <ernopp@users.noreply.github.com>
2019-11-09 16:55:52 +0000

    Homepage example image

--- twitter-client.js
+++ twitter-client.js
@@ -1,11 +1,15 @@
 /* Twitter client - app settings: https://apps.twitter.com/app/5573851/settings */
 
+const debug = require('debug')('lessnoise:twitter-client')
+
 const oauth = require('oauth')
 
 const _twitterConsumerKey = process.env.KEY
 const _twitterConsumerSecret = process.env.SECRET
 const callbackString = process.env.NODE_ENV === "production" ? "https://glacial-ravine-68340.herokuapp.com/signin/callback" :
     "http://localhost:" + process.env.PORT  + "/signin/callback"
+
+debug(callbackString)
 
 module.exports = new oauth.OAuth(
   "https://twitter.com/oauth/request_token", "https://twitter.com/oauth/access_token",
