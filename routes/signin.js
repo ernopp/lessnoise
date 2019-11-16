a4e767e98d46f534d5ca70bf6e77699d2d4f360c
Ernest Oppetit <ernopp@users.noreply.github.com>
2019-11-16 17:00:59 +0000

    Open links in new tab

--- routes/signin.js
+++ routes/signin.js
@@ -1,50 +1,51 @@
 //Routes oauth flow connect and callback
 
-const consumer = require('../twitter-client');
-const express = require('express');
-const router = express.Router();
-const app = require('../app');
+const consumer = require('../twitter-client')
+const express = require('express')
+const router = express.Router()
+const app = require('../app')
 const debug = require('debug')('lessnoise:signin')
-const utils = require('../utils');
+const verbosedebug = require('debug')('lessnoise-verbose:signin')
+const utils = require('../utils')
 
 router.get('/', function(req, res, next){
   consumer.getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
     if (error) {
       debug("error getting oauth request token" + inspect(error))
       next(error)
-      // res.send("Error getting OAuth request token : " + inspect(error), 500);
+      // res.send("Error getting OAuth request token : " + inspect(error), 500)
     } else {  
-      req.session.oauthRequestToken = oauthToken;
-      req.session.oauthRequestTokenSecret = oauthTokenSecret;
-      // req.session.save();
-      debug("Calling out to twitter for first oauth");
-      debug("------------------------");
-      debug("Session id is: " + req.sessionID);
-      debug("<<"+req.session.oauthRequestToken);
-      debug("<<"+req.session.oauthRequestTokenSecret);
-      res.redirect("https://twitter.com/oauth/authorize?oauth_token="+req.session.oauthRequestToken);      
+      req.session.oauthRequestToken = oauthToken
+      req.session.oauthRequestTokenSecret = oauthTokenSecret
+      // req.session.save()
+      verbosedebug("Calling out to twitter for first oauth")
+      verbosedebug("------------------------")
+      verbosedebug("Session id is: " + req.sessionID)
+      verbosedebug("<<"+req.session.oauthRequestToken)
+      verbosedebug("<<"+req.session.oauthRequestTokenSecret)
+      res.redirect("https://twitter.com/oauth/authorize?oauth_token="+req.session.oauthRequestToken)      
     }
-  });
-});
+  })
+})
 
 router.get('/callback', function(req, res, next){
-    debug("Executing callback");
-  debug("------------------------");
-  debug("Session id is: " + req.sessionID);
-  debug("token>>"+req.session.oauthRequestToken);
-  debug("secret>>"+req.session.oauthRequestTokenSecret);
-  debug("oauth_verifier>>"+req.query.oauth_verifier);
+  verbosedebug("Executing callback")
+  verbosedebug("------------------------")
+  verbosedebug("Session id is: " + req.sessionID)
+  verbosedebug("token>>"+req.session.oauthRequestToken)
+  verbosedebug("secret>>"+req.session.oauthRequestTokenSecret)
+  verbosedebug("oauth_verifier>>"+req.query.oauth_verifier)
   consumer.getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
     if (error) {
-      // res.send("Error getting OAuth access token : " + inspect(error) + "[" + oauthAccessToken + "]" + "[" + oauthAccessTokenSecret + "]" + "[" + inspect(results) + "]", 500);
+      // res.send("Error getting OAuth access token : " + inspect(error) + "[" + oauthAccessToken + "]" + "[" + oauthAccessTokenSecret + "]" + "[" + inspect(results) + "]", 500)
       next(error)
     } else {
-      req.session.oauthAccessToken = oauthAccessToken;
-      req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
+      req.session.oauthAccessToken = oauthAccessToken
+      req.session.oauthAccessTokenSecret = oauthAccessTokenSecret
       
-      res.redirect('/');
+      res.redirect('/')
     }
-  });
-});
+  })
+})
 
-module.exports = router;
+module.exports = router
